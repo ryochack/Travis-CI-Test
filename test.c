789f725068aaa8d93e318866eeabbf0d00b7ca3e
ryochack <ryochack@gmail.com>
2016-02-01 02:27:19 +0900

    added test.c

---
+++ test.c
@@ -0,0 +1,28 @@
+#if defined(_WIN32)
+#warning _WIN32
+#endif
+
+#if defined(__unix__)
+#warning __unix__
+#endif
+
+#if defined(__unix)
+#warning __unix
+#endif
+
+#if defined(unix)
+#warning unix
+#endif
+
+#if defined(__APPLE__)
+#warning __APPLE__
+#endif
+
+#if defined(__MACH__)
+#warning __MACH__
+#endif
+
+
+
+
+
